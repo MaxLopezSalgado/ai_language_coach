199232c6bef8575129edd0d4a8578227c328d89b
MaxLopezSalgado <maxlopezsalgado@gmail.com>
2023-08-08 17:41:57 +0200

    typo

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ def home():
     return render_template("index.html", **locals())
  
 if __name__ == "__main__":
-    app.run(host="0.0.0 .0", port="8000", debug=True )
+    app.run(host="0.0.0.0", port="8000", debug=True )
 
